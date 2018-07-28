86989e4b4196158195225cc8155c45baa16e48bd
Thomas Urban <thomas.urban@cepharum.de>
2018-07-28 12:10:18 +0200

    revising notation

--- src/Resources/contao/languages/de/tl_page.php
+++ src/Resources/contao/languages/de/tl_page.php
@@ -1,5 +1,5 @@
 <?php
 
 $GLOBALS['TL_LANG']['tl_page']['position_legend'] = 'Geo-Koordinaten';
-$GLOBALS['TL_LANG']['tl_page']['lon'] = ['Längengrad', 'Wertebereich: -180 bis +180'];
-$GLOBALS['TL_LANG']['tl_page']['lat'] = ['Breitengrad', 'Wertebereich: -180 bis +180'];
+$GLOBALS['TL_LANG']['tl_page']['lon'] = array( 'Längengrad', 'Wertebereich: -180 bis +180' );
+$GLOBALS['TL_LANG']['tl_page']['lat'] = array( 'Breitengrad', 'Wertebereich: -180 bis +180' );
