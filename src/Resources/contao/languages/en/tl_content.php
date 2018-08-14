868438115306321467d26add6818046981d96eac
Thomas Urban <thomas.urban@cepharum.de>
2018-08-14 09:31:43 +0200

    revising wording and order of options

--- src/Resources/contao/languages/en/tl_content.php
+++ src/Resources/contao/languages/en/tl_content.php
@@ -4,8 +4,8 @@ $GLOBALS['TL_LANG']['tl_content']['area_legend'] = 'Output Area';
 $GLOBALS['TL_LANG']['tl_content']['area'] = array( 'Output Area', 'Select area on page with travel description this element belongs to.' );
 
 $GLOBALS['TL_LANG']['tl_content']['area_auto'] = 'automatic';
-$GLOBALS['TL_LANG']['tl_content']['area_banner'] = 'Images';
+$GLOBALS['TL_LANG']['tl_content']['area_gallery'] = 'Images';
 $GLOBALS['TL_LANG']['tl_content']['area_description'] = 'Description';
-$GLOBALS['TL_LANG']['tl_content']['area_plan'] = 'Schedule';
+$GLOBALS['TL_LANG']['tl_content']['area_itinerary'] = 'Itinerary';
 $GLOBALS['TL_LANG']['tl_content']['area_example'] = 'Examples';
 $GLOBALS['TL_LANG']['tl_content']['area_prices'] = 'Prices';
